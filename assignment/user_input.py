3a1a5052448e46f7caeae3e33b459322d5e1d194
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-22 10:35:55 +0100

    update

--- assignment/user_input.py
+++
@@ -1,6 +0,0 @@
-
-name = input("What is your name? ")
-age = input("How old are you? ")
-location = input("Where are you located? ")
-
-print(f"Hello {name}, you are {age} years old and live in {location}.")
