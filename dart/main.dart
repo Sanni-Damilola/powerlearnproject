b41978e2f1a1c43e42fe2262ada45808846c53af
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-22 17:53:54 +0100

    feat: Add more Dart code snippets
    Updated Dart code snippets with additional comments and organized function names for better clarity and understanding.

--- dart/main.dart
+++ dart/main.dart
@@ -1,17 +1,14 @@
 void main() {
-  // Define variables
   String name = "Sanni";
   int age = 30;
   String nationality = "Nigeria";
   bool isMarried = false;
 
-  // Print individual details
   print("Name: $name");
   print("Age: $age");
   print("Nationality: $nationality");
   print("Marital Status: ${isMarried ? 'Married' : 'Single'}");
 
-  // Print summary message
   print(
       "Hello! My name is $name. I am $age years old, and I am from $nationality.");
 }
