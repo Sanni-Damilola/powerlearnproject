4627f5bf332c93762cc3b7da3dc6d6398919a1f1
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-22 17:43:55 +0100

    feat: dart

---
+++ dart/main.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Sanni";
+  int age = 30;
+  String nationality = "Nigeria";
+  bool isMarried = false;
+
+  print(name);
+  print(age);
+  print(nationality);
+  print(isMarried);
+
+  print("Hello My Name is $name, I'm $age year old, I love $nationality");
+}
