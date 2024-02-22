caa772d5fce991721f721e917f7d82eac4db13c7
Sanni-Damilola <sannifortune11@gmail.com>
2024-02-22 18:03:07 +0100

    feat: Add more Dart code snippets
    Updated Dart code snippets with additional comments and organized function names for better clarity and understanding.

--- dart/function.dart
+++ dart/function.dart
@@ -1,8 +1,37 @@
-int add(int a, int b) {
-  return a + b;
+double calculateRectangleArea(double length, double width) {
+  return length * width;
+}
+
+void printRectangleArea(double length, double width) {
+  double area = calculateRectangleArea(length, width);
+  print("The area of the rectangle is: $area");
+}
+
+bool isEven(int number) {
+  return number % 2 == 0;
+}
+
+void printIsEven(int number) {
+  print("$number is even: ${isEven(number)}");
+}
+
+void greet(String name) {
+  print("Hello, $name!");
+}
+
+void introduceYourself(String name, int age, String nationality) {
+  print(
+      "Hello! My name is $name. I am $age years old, and I am from $nationality.");
 }
 
 void main() {
-  int result = add(5, 3);
-  print(result);
+  printRectangleArea(5.0, 3.0); // Output: The area of the rectangle is: 15.0
+
+  printIsEven(4); // Output: 4 is even: true
+  printIsEven(3); // Output: 3 is even: false
+
+  greet("Alice"); // Output: Hello, Alice!
+
+  introduceYourself("Sanni", 30,
+      "Nigeria"); // Output: Hello! My name is Sanni. I am 30 years old, and I am from Nigeria.
 }
